51b52cb3d1d32cc3985126fb57578ebb0b27621a
garynaruto <garynaruto@gmail.com>
2016-03-09 20:25:27 +0800

    分類元件完成

---
+++ New Unity Project 3/Assets/chartInfo/chartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class chartScript: MonoBehaviour {
+	public Image image;
+	void Start () {
+		image.fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (image.fillAmount < 1) {
+			image.fillAmount += 0.01f;
+		} else {
+			image.fillAmount = 0;
+		}
+
+	}
+}
