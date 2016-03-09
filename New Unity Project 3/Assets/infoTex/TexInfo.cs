e279a0372342d382788837579bf20a9624722b56
garynaruto <garynaruto@gmail.com>
2016-03-09 19:27:03 +0800

    比賽用Unity 介面設計

---
+++ New Unity Project 3/Assets/infoTex/TexInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TexInfo: MonoBehaviour {
+
+	public Text text;
+
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		int i = Random.Range (100000, 10000000);
+		text.text =""+i; 
+	}
+}
---
+++ New Unity Project 3/Assets/texbox/TextBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+	public class TextBox : MonoBehaviour {
+	int count=0;
+	public Text text;
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		count++;
+		if(count<250){
+			int i = Random.Range (0, 9);
+			text.text +=""+i; 
+		}
+	}
+
+}
